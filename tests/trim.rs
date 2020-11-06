290a360dcf7ab3c83b69488fdb80acd75b2f3654
muji <muji@tmpfs.org>
2020-11-06 15:39:23 +0800

    Add failing trim tests.

---
+++ tests/trim.rs
@@ -0,0 +1,63 @@
+use bracket::{
+    error::{Error, SyntaxError},
+    Registry, Result,
+};
+use serde_json::json;
+
+static NAME: &str = "vars.rs";
+
+#[test]
+fn trim_statement() -> Result<()> {
+    let mut registry = Registry::new();
+    let value = r"
+{{~foo~}}
+";
+    let data = json!({"foo": "bar"});
+    let result = registry.once(NAME, value, &data)?;
+    assert_eq!("bar", &result);
+    Ok(())
+}
+
+#[test]
+fn trim_before_block_start() -> Result<()> {
+    let mut registry = Registry::new();
+    let value = r"
+{{~#if true}}{{foo}}{{/if}}";
+    let data = json!({"foo": "bar"});
+    let result = registry.once(NAME, value, &data)?;
+    assert_eq!("bar", &result);
+    Ok(())
+}
+
+#[test]
+fn trim_after_block_start() -> Result<()> {
+    let mut registry = Registry::new();
+    let value = r"{{#if true~}}
+{{foo}}{{/if}}";
+    let data = json!({"foo": "bar"});
+    let result = registry.once(NAME, value, &data)?;
+    assert_eq!("bar", &result);
+    Ok(())
+}
+
+#[test]
+fn trim_before_block_end() -> Result<()> {
+    let mut registry = Registry::new();
+    let value = r"{{#if true}}{{foo}}
+{{~/if}}";
+    let data = json!({"foo": "bar"});
+    let result = registry.once(NAME, value, &data)?;
+    assert_eq!("bar", &result);
+    Ok(())
+}
+
+#[test]
+fn trim_after_block_end() -> Result<()> {
+    let mut registry = Registry::new();
+    let value = r"{{#if true}}{{foo}}{{/if~}}
+";
+    let data = json!({"foo": "bar"});
+    let result = registry.once(NAME, value, &data)?;
+    assert_eq!("bar", &result);
+    Ok(())
+}
