0c341556659f40ee8360a751b07ffadd69725577
muji <muji@tmpfs.org>
2020-11-25 11:18:32 +0800

    Tidy template module.

--- src/template.rs
+++ src/template.rs
@@ -15,6 +15,9 @@ use crate::{
 
 use rental::rental;
 
+/// Collection of named templates.
+pub type Templates<'a> = HashMap<String, Template>;
+
 rental! {
     mod rentals {
         use super::*;
@@ -28,7 +31,7 @@ rental! {
 
 // SEE: https://github.com/projectfluent/fluent-rs/blob/master/fluent-bundle/src/resource.rs#L5-L14
 
-/// Template resource is a template that owns the underlying string.
+/// Template that owns the underlying string and a corresponding document node.
 #[derive(Debug)]
 pub struct Template(rentals::Template);
 
@@ -91,70 +94,3 @@ impl fmt::Display for Template {
     }
 }
 
-/// Collection of named templates.
-pub type Templates<'a> = HashMap<String, Template>;
-
-/*
-
-/// Type that adds rendering capability to a document node.
-#[derive(Debug, Default)]
-pub struct Template<'source> {
-    node: Node<'source>,
-}
-
-impl<'source> Template<'source> {
-    /// Create a new template.
-    pub(crate) fn new(node: Node<'source>) -> Self {
-        Self { node }
-    }
-
-    /// The document node for the template.
-    pub fn node(&self) -> &'source Node {
-        &self.node
-    }
-
-    /// Compile a block.
-    pub fn compile<'a>(
-        source: &'a str,
-        options: ParserOptions,
-    ) -> SyntaxResult<Template<'a>> {
-        let mut parser = Parser::new(source, options);
-        let node = parser.parse()?;
-        Ok(Template::new(node))
-    }
-
-    /// Render this template to the given writer.
-    pub(crate) fn render<'a, T>(
-        &self,
-        strict: bool,
-        escape: &EscapeFn,
-        helpers: &'a HelperRegistry<'a>,
-        templates: &'a Templates<'a>,
-        name: &str,
-        data: &T,
-        writer: &'a mut impl Output,
-    ) -> RenderResult<()>
-    where
-        T: Serialize,
-    {
-        let mut rc = Render::new(
-            strict,
-            escape,
-            helpers,
-            templates,
-            name,
-            data,
-            Box::new(writer),
-        )?;
-
-        rc.render(&self.node)
-    }
-}
-
-impl fmt::Display for Template<'_> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.node.fmt(f)
-    }
-}
-
-*/
