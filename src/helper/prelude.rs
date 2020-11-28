08d01dddab55b5509b2fde34ddb0539cf13a6ebc
muji <muji@tmpfs.org>
2020-11-28 09:18:04 +0800

    Add HelperResult to the helper prelude.

--- src/helper/prelude.rs
+++ src/helper/prelude.rs
@@ -1,7 +1,7 @@
 //! Prelude for helper definitions.
 pub use crate::{
     error::HelperError,
-    helper::{Helper, HelperValue, LocalHelper},
+    helper::{Helper, HelperValue, LocalHelper, HelperResult},
     parser::ast::Node,
     render::{Context, Render, Scope, Type},
 };
