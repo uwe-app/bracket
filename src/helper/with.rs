86d3aa6bab17dbb84d210946ac2d342fa30eb6ae
muji <muji@tmpfs.org>
2021-01-09 11:44:53 +0800

    With helper does not execute for null values.

--- src/helper/with.rs
+++ src/helper/with.rs
@@ -5,6 +5,8 @@ use crate::{
     render::{Context, Render, Scope},
 };
 
+use serde_json::Value;
+
 /// Set the scope for a block to the target argument.
 pub struct With;
 
@@ -17,13 +19,18 @@ impl Helper for With {
     ) -> HelperValue {
         ctx.arity(1..1)?;
 
-        if let Some(template) = template {
-            rc.push_scope(Scope::new());
-            if let Some(ref mut scope) = rc.scope_mut() {
-                scope.set_base_value(ctx.get(0).cloned().unwrap());
+        if let Some(arg) = ctx.get(0) {
+            let is_null = if let Value::Null = arg { true } else { false };
+            if !is_null {
+                if let Some(template) = template {
+                    rc.push_scope(Scope::new());
+                    if let Some(ref mut scope) = rc.scope_mut() {
+                        scope.set_base_value(ctx.get(0).cloned().unwrap());
+                    }
+                    rc.template(template)?;
+                    rc.pop_scope();
+                }
             }
-            rc.template(template)?;
-            rc.pop_scope();
         }
 
         Ok(None)
